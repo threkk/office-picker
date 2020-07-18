410a9162606834834f3812bf9dfb6591f58b73ed
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2020-07-18 15:31:06 +0200

    feat: Sets a basic layout for the project.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,106 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+        <h1>Adyen Office Picker</h1>
+    <div class="content">
+      <div class="columns">
+        <div class="column col-sm-12 col-md-6 col-xl-4 col-3">
+          <div class="card my-2">
+            <div class="card-image">
+              <img src="./assets/logo.png" class="img-responsive">
+            </div>
+            <div class="card-header">
+              <div class="card-title h5">Lorem ipsum</div>
+              <div class="card-subtitle text-gray">Dolor sit amet</div>
+            </div>
+            <div class="card-body">
+              ...consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary">Okay</button>
+            </div>
+          </div>
+        </div>
+
+
+        <!-- Card end -->
+        <div class="column col-sm-12 col-md-6 col-xl-4 col-3">
+          <div class="card my-2">
+            <div class="card-image">
+              <img src="./assets/logo.png" class="img-responsive">
+            </div>
+            <div class="card-header">
+              <div class="card-title h5">Lorem ipsum</div>
+              <div class="card-subtitle text-gray">Dolor sit amet</div>
+            </div>
+            <div class="card-body">
+              ...consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary">Okay</button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card end -->
+        <div class="column col-sm-12 col-md-6 col-xl-4 col-3">
+          <div class="card my-2">
+            <div class="card-image">
+              <img src="./assets/logo.png" class="img-responsive">
+            </div>
+            <div class="card-header">
+              <div class="card-title h5">Lorem ipsum</div>
+              <div class="card-subtitle text-gray">Dolor sit amet</div>
+            </div>
+            <div class="card-body">
+              ...consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary">Okay</button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card end -->
+        <div class="column col-sm-12 col-md-6 col-xl-4 col-3">
+          <div class="card my-2">
+            <div class="card-image">
+              <img src="./assets/logo.png" class="img-responsive">
+            </div>
+            <div class="card-header">
+              <div class="card-title h5">Lorem ipsum</div>
+              <div class="card-subtitle text-gray">Dolor sit amet</div>
+            </div>
+            <div class="card-body">
+              ...consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary">Okay</button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card end -->
+      </div> 
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import { Vue, Component } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+$primary-color: rgb(10, 191, 83);
+
+@import '../node_modules/spectre.css/src/spectre';
+
+body {
+  margin: 20px auto;
+  max-width: 1200px;
+  line-height: 1.6;
+  padding: 0 10px;
 }
+
 </style>
